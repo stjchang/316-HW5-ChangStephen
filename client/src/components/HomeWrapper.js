61167488b04d436c415015b65b350ad200cb78ca
stjchang <changstephen289@gmail.com>
2025-12-05 03:32:50 -0500

    added comp back

---
+++ client/src/components/HomeWrapper.js
@@ -0,0 +1,13 @@
+import { useContext } from 'react'
+import HomeScreen from './HomeScreen'
+import SplashScreen from './SplashScreen'
+import AuthContext from '../auth'
+
+export default function HomeWrapper() {
+    const { auth } = useContext(AuthContext);
+    
+    if (auth.loggedIn)
+        return <HomeScreen />
+    else
+        return <SplashScreen />
+}
